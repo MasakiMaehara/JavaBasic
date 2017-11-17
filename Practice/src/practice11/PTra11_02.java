1aeb9c666f5b80e771d83d8b7e311a60cec526df
前原勝貴 <09012806274mm@gmail.com>
2017-11-17 17:46:05 +0900

    課題１２まで提出

--- Practice/src/practice11/PTra11_02.java
+++ Practice/src/practice11/PTra11_02.java
@@ -1,4 +1,7 @@
 package practice11;
+
+import java.util.Scanner;
+
 /*
  * PTra11_02.java
  *   作成	LIKEIT	2017
@@ -10,11 +13,22 @@ public class PTra11_02 {
 	public static void main(String[] args) {
 
 		// ★ ReadFileClassのクラスメソッドreadBookDataFile()を使い、本情報を取得します
-
+			Book[] books = FileReaderClass.readBookDataFile();
 
 		System.out.println("探したい本のタイトル（又はその一部）を入力してください");
 
 		// ★ ユーザが入力した文字列が、本情報のタイトル一部に含まれていた場合は、その本情報を出力してください
+		Scanner scan = new Scanner(System.in);
+	    String str = scan.next();
+
+		for (int i = 0; i < books.length; i++) {
+			if (books[i].title.indexOf(str) != -1) {
+			    // 部分一致です
+
+				System.out.println(books[i].dispBookInfo());
+			}
+
+		}
 
 	}
 }
