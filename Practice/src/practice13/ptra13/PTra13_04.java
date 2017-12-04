d500d90fa21897e99dfa5a0510f3786c2fdc87aa
前原勝貴 <09012806274mm@gmail.com>
2017-12-04 18:31:50 +0900

    課題13の５まで提出

--- Practice/src/practice13/ptra13/PTra13_04.java
+++ Practice/src/practice13/ptra13/PTra13_04.java
@@ -6,6 +6,9 @@
  */
 package practice13.ptra13;
 
+import practice13.common.Hero;
+import practice13.common.Slime;
+
 public class PTra13_04 {
 
 	/*
@@ -15,17 +18,33 @@ public class PTra13_04 {
 	public static void main(String[] args) {
 
 		// ★ HeroインスタンスとSlimeインスタンスを作成し、それぞれの名前に"勇者", "スライム"を設定してください
-
-
+		Hero hero = new Hero();
+		Slime slime = new Slime();
+		hero.setName("勇者");
+		slime.setName("スライム");
 		/*
 		 * ★ HeroとSlimeを、どちらかが体力０になるまで戦わせます
 		 *
 		 * 	●Heroの攻撃 -> ダメージ判定 -> Slimeの攻撃 -> ダメージ判定
-		 * 上記を繰り返し行います
-		 */
+		 * 上記を繰り返し行います*/
+		while(true) {
 
+		boolean slimeDead =  slime.damage(hero.attack());
 
-		// ★ 勝利した方の出力を行ってください。「○○は■■との戦闘に勝利した」
+		if(slimeDead) {
+			System.out.println(hero.getName()+"は"+slime.getName()+"に勝利した。");
+			break;
+		}
+		boolean heroDead =hero.damage(slime.attack());
+		if(heroDead) {
+			System.out.println(slime.getName()+"は"+hero.getName()+"に勝利した。");
+			break;
+		}
 
+		 /* ダメージ判定を行います
+		 * @param attack 攻撃値
+		 * @return	体力が0になったらtrue、まだ体力が残っていたらfalse
+		 */		// ★ 勝利した方の出力を行ってください。「○○は■■との戦闘に勝利した」
+		}
 	}
-}
+	}
